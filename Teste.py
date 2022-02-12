89e981298e56645d41ef95a7fccf97ae4204a421
Vinicius Filipe <99560879+Vinicius26Filipe70@users.noreply.github.com>
2022-02-12 19:10:34 -0300

    Aula12/02/22
    Aprendendo os comandos iniciais e alguns exercícios.

---
+++ Teste.py
@@ -0,0 +1,11 @@
+#Tenho que calcular a hipotenusa dado os catetos 3 e 2
+x=pow(2,2)+pow(3,2)
+print(x**(1/2))
+#Crie um script que leia um nome e digite uma mensagem com o valor digitado
+y=input("Qual é o seu nome?")
+print("Ola " + y + " é um prazer te conhecer")
+#Crie um script que leia o dia, o mês e o ano de uma pessoa e depois crie uma mensagem a data formada
+d=input("Qual é o dia do seu nascimento?")
+m=input("Qual é o mês do seu nascimento?")
+a=input("Qual é o ano do seu nascimento?")
+print(f"{d}/{m}/{a}")
